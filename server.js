561a94a59c19b955cd4fe8be7b2bea9c343181f1
Bondifrench <d_dumaine@yahoo.com>
2014-05-09 14:51:45 +1000

    adding route middleware

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env Node
 
 //Base Setup
-
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 8080;
@@ -9,6 +8,16 @@ var port = process.env.PORT || 8080;
 // $routeParams
 //We'll define our routes here
 var router = express.Router();
+//route middleware that will happen on every request
+router.use(function (req, res, next) {
+	//log each request to the console
+	console.log(req.method, req.url);
+	//continue doing what we were doing and go to the route 
+	next();
+})
+//The order you place your middleware and routes is very important. Everything will happen in the order
+//that they appear. This means that if you place your middleware after a route, then the route will happen
+// before the middleware and the request will end there. Your middleware will not run at that point.
 
 //home page route (http://localhost:8080)
 router.get("/", function (req, res) {
@@ -20,10 +29,16 @@ router.get("/about", function (req, res) {
 });
 
 //apply the routes to our application
-
 app.use("/", router);
 
-//Start the server
+//Notice how we can set a default root for using these routes we just defined.
+//If we had changed line 21 to app.use('/app', router), then our routes would be http://localhost:8080/app
+// and http://localhost:8080/app/about.
 
+//This is very powerful because we can create multiple express.Router()s and then apply them to our application.
+//We could have a Router for our basic routes, authenticated routes, and even API routes.
+
+
+//Start the server
 app.listen(port);
 console.log("Server started on port: "+ port);
